1c2969119878e83eb406ffaeb8994d88930d4348
Marlon <marlonpo@gmail.com>
2021-05-29 06:46:14 -0300

    Subindo

---
+++ js/scrips.js
@@ -0,0 +1,18 @@
+/* mascaras*/
+function mascaraPreta(){
+    document.getElementById('mascara-preta').style.display = 'block';        
+}
+function fecharMascaraPreta(){
+    document.getElementById('mascara-preta').style.display = 'none';        
+}
+function mascaraBranca(){
+    document.getElementById('mascara-branca').style.display = 'block';        
+}
+function fecharMascaraBranca(){
+    document.getElementById('mascara-branca').style.display = 'none';        
+}
+/*fim mascaras*/
+
+let escolhaCamisa
+
+
