d18849fd2e653c2c601c8bb403d53302c2bd6f4c
Marlon Oliveira <marlonpo@gmail.com>
2021-05-29 16:52:50 -0300

    Finalização da pagina Camisas

--- js/scrips.js
+++ js/scrips.js
@@ -13,55 +13,58 @@ function fecharMascaraBranca(){
 }
 /*fim mascaras*/
 
-let escolhaCamisa;
-let textoModeloCamisa;
-let menuCamisas;
 
-switch(menuCamisas){
-    case 'lisa': 
-        escolhaCamisa = '../img/ModeloLisa.png'
-        textoModeloCamisa = 'Camisa <br> Lisa'        
-        break
-    case 'mercurio':
-        escolhaCamisa = "../img/ModeloMercurio.png";
-        textoModeloCamisa = 'Camisa de Mercúrio';
-        break
-    case 'venus':
-        escolhaCamisa = "../img/ModeloVenus.png";
-        textoModeloCamisa = 'Camisa<br> de Vênus';
-        break
-    case 'terra':
-        escolhaCamisa = "../img/ModeloTerra.png";
-        textoModeloCamisa = 'Camisa<br> de Terra';
-        break
-    case 'marte':
-        escolhaCamisa = "../img/ModeloMarte.png";
-        textoModeloCamisa = 'Camisa<br> de Marte';
-        break
-    case 'jupiter':
-        escolhaCamisa = "../img/ModeloJupiter.png";
-        textoModeloCamisa = 'Camisa<br> de Jupiter';
-        break
-    case 'saturno':
-        escolhaCamisa = "../img/ModeloSaturno.png";
-        textoModeloCamisa = 'Camisa <br> de Saturno';
-        break
-    case 'urano':
-        escolhaCamisa = "../img/ModeloUrano.png";
-        textoModeloCamisa = 'Camisa <br> de Urano';
-        break
-    case 'netuno':
-        escolhaCamisa = "../img/ModeloNetuno.png";
-        textoModeloCamisa = 'Camisa <br> de Netuno';
-        break
-    case 'plutao':
-        escolhaCamisa = "../img/ModeloPlutao.png";
-        textoModeloCamisa = 'Camisa <br> de Plutão';
-        break
-    
+function mostrarCamisa(modelo){
+
+    let escolhaCamisa;
+    let textoModeloCamisa;
+        
+    switch(modelo){
+        case 'lisa': 
+            escolhaCamisa = '../img/ModeloLisa.png'
+            textoModeloCamisa = 'Camisa <br> Lisa'        
+            break
+        case 'mercurio':
+            escolhaCamisa = "../img/ModeloMercurio.png";
+            textoModeloCamisa = 'Camisa<br> de Mercúrio';
+            break
+        case 'venus':
+            escolhaCamisa = "../img/ModeloVenus.png";
+            textoModeloCamisa = 'Camisa<br> de Vênus';
+            break
+        case 'terra':
+            escolhaCamisa = "../img/ModeloTerra.png";
+            textoModeloCamisa = 'Camisa<br> de Terra';
+            break
+        case 'marte':
+            escolhaCamisa = "../img/ModeloMarte.png";
+            textoModeloCamisa = 'Camisa<br> de Marte';
+            break
+        case 'jupiter':
+            escolhaCamisa = "../img/ModeloJupiter.png";
+            textoModeloCamisa = 'Camisa<br> de Jupiter';
+            break
+        case 'saturno':
+            escolhaCamisa = "../img/ModeloSaturno.png";
+            textoModeloCamisa = 'Camisa <br> de Saturno';
+            break
+        case 'urano':
+            escolhaCamisa = "../img/ModeloUrano.png";
+            textoModeloCamisa = 'Camisa <br> de Urano';
+            break
+        case 'netuno':
+            escolhaCamisa = "../img/ModeloNetuno.png";
+            textoModeloCamisa = 'Camisa <br> de Netuno';
+            break
+        case 'plutao':
+            escolhaCamisa = "../img/ModeloPlutao.png";
+            textoModeloCamisa = 'Camisa <br> de Plutão';
+            break
+        
+    }
+    document.getElementById('imagemModeloCamisa').src = escolhaCamisa;
+    document.getElementById('textoModeloCamisa').innerHTML = textoModeloCamisa;
+
+
 }
-console.log(textoModeloCamisa)
-console.log(escolhaCamisa)
-//document.getElementById('imagemModeloCamisa').src = escolhaCamisa;
-//document.getElementById('textoModeloCamisa').innerHTML = textoModeloCamisa;
 
